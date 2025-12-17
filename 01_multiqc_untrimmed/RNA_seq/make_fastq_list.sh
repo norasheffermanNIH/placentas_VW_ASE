7c5017a29c2ffec7a61d155ea645af9862edb087
Nora Shefferman <sheffermannm@nih.gov>
2025-12-17 16:09:33 -0500

    Organize multiqc directory

--- 01_multiqc_untrimmed/RNA_seq/make_fastq_list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#Add FASTQs from Q1
-find /data/Wilson_Lab/placenta/valleywise_raw_data/RNAseq -type f -name "*.fastq.gz" | sort >> fastq_list.txt
-
-#Add merged FASTQs from Q2-4
-find /data/Wilson_Lab/projects/placentas_VW_ASE/01_multiqc_untrimmed/RNA_seq/merged_fastqs -type f -name "*.fastq.gz" | sort >> fastq_list.txt
--- 01_multiqc_untrimmed/methyl_seq/make_fastqc_swarm_XY.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-OUTPUT_DIR=/data/Wilson_Lab/projects/placentas_VW_ASE/01_multiqc_untrimmed/methyl_seq/fastqc_results_XY/
-mkdir -p $OUTPUT_DIR
-
-cat fastq_list_XY.txt | sed "s|^|fastqc -o $OUTPUT_DIR |" > fastqc_XY.swarm
